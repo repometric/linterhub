4a2dc0e4289189c0744468af279f021f74814d8c
xferra <xferra@live.com>
2016-09-05 14:09:10 +0300

    Reach target of 100 linters

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ for dir in "dockers/$basedir"/*; do
     if test -d "$dir"; then
         dir="$(basename $dir)"  # Returns just "to"
         mkdir -p tests/$dir
+        echo "Test: $dir"
         sh linter.sh engine:build "$dir" > tests/"$dir"/build.txt
         sh linter.sh engine:save "$dir" > tests/"$dir"/image.tgz
     fi
