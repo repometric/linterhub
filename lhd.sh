980a2206e008f8652e748996bec7688699d08639
xferra <xferra@live.com>
2016-10-28 00:46:18 +0300

    Fix quotes for lhd command

--- lhd.sh
+++ lhd.sh
@@ -1,2 +1,2 @@
 # docker build -t linterhub .
-docker run -i -v /var/run/docker.sock:/var/run/docker.sock linterhub /bin/bash lh.sh $@
+docker run -i -v /var/run/docker.sock:/var/run/docker.sock linterhub /bin/bash lh.sh "$@"
