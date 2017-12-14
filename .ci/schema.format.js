a0e245344923867b56e0503865848a1036c97f36
xferra <xferra@live.com>
2017-12-14 16:37:40 +0300

    Update formatting

--- .ci/schema.format.js
+++ .ci/schema.format.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
 const finder = require('fs-finder');
 const files = finder.findFiles('hub/**/*.json');
 const results = files.map(function(fileName) {
     const content = fs.readFileSync(fileName).toString();
     const json = JSON.parse(content);
     const formatted = JSON.stringify(json, null, 4);
+    console.log(`Format: ${fileName}`);
     fs.writeFileSync(fileName, formatted + '\n');
 });
