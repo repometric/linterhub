1ea8c10428093d24e4ad3f71b87e0bc06261e5c2
Gleb Berjoskin <TrulyIrishDancer@yandex.ru>
2018-04-28 23:12:55 +0300

    Improve code quality in reporter.js for jshint

--- engine/jshint/reporter.js
+++ engine/jshint/reporter.js
@@ -1,39 +1,40 @@
 "use strict";
 module.exports = {
+    resultsFunction(record){
+        let error = record.error;
+        let severityString = "";
+        if (!files[record.file]) {
+            files[record.file] = [];
+        }
+
+        switch (error.code[0]) {
+            case "I":
+                severityString = "info";
+                break;
+            case "W":
+                severityString = "warning";
+                break;
+            default:
+            case "E":
+                severityString = "error";
+                break;
+        }
+
+        files[record.file].push({
+            message: error.reason,
+            severity: severityString,
+            line: error.line,
+            lineEnd: error.line,
+            column: error.character,
+            ruleId: "jshint:" + error.code,
+            ruleName: error.id,
+            ruleNamespace: error.scope,
+        });
+    },
 	reporter(results, data, opts) {
         let files = {};
 		opts = opts || {};
-        results.forEach((record) => {
-            let error = record.error;
-            let severityString = "";
-            if (!files[record.file]) {
-                files[record.file] = [];
-            }
-
-            switch (error.code[0]) {
-                case "I":
-                    severityString = "info";
-                    break;
-                case "W":
-                    severityString = "warning";
-                    break;
-                default:
-                case "E":
-                    severityString = "error";
-                    break;
-            }
-
-            files[record.file].push({
-                message: error.reason,
-                severity: severityString,
-                line: error.line,
-                lineEnd: error.line,
-                column: error.character,
-                ruleId: "jshint:" + error.code,
-                ruleName: error.id,
-                ruleNamespace: error.scope,
-            });
-        });
+        results.forEach(this.resultsFunction(record));
         process.stdout.write(JSON.stringify(Object.keys(files).map(function (key) {
             return {
                 path: key,
