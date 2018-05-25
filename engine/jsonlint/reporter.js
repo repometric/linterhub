efef6105175fbe15848e10d2c5e4525e54cf6c02
Roman <romanrostislavovich@gmail.com>
2018-05-25 15:57:26 +0300

    Implement reporter for jsonlint engine

---
+++ engine/jsonlint/reporter.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const template = module.exports = {
+    run: function(onFinish) {
+        process.stdin.resume();
+        process.stdin.setEncoding('utf8');
+
+        let lingeringLine = '';
+        let lines = [];
+        
+        process.stdin.on('data', function(chunk) {
+            let linesParsed = chunk.split('\n');
+            linesParsed[0] = lingeringLine + linesParsed[0];
+            lingeringLine = linesParsed.pop();
+            linesParsed.forEach((line) => {
+                if (line !== null) {
+                    lines.push(line);
+                }
+            });
+        });
+
+        process.stdin.on('end', function() {
+            onFinish(lines);
+        });
+      
+    },
+};
+
+const reporter = module.exports = {
+    run: function(regex, convertMatch) {
+        let results = [];
+
+        template.run(function(lines) {
+            lines.forEach(processLine);
+           console.log(JSON.stringify(results));
+        });
+       
+        /**
+         * Generates standard model form each line
+         * @param {*} line Line to parse
+         */
+        function processLine(line) {
+            //console.log(line);
+            let match = new RegExp(regex).exec(line);
+
+            if (match === null) {
+                return;
+            }
+ 
+            let objParsed = convertMatch(match);
+
+            let obj = results.find(function(element) {
+                return objParsed.path === element.path;
+            });
+
+            if (obj === undefined) {
+                obj = {
+                    path: objParsed.path,
+                    messages: [],
+                };
+                results.push(obj);
+            }
+
+            obj.messages.push(objParsed.message);
+        }
+    },
+};
+let regex = /(.*): line ([0-9]+), col ([0-9]+), (.*)/g;
+
+reporter.run(regex, function(match) {
+    return {
+        path: match[1].trim(),
+        message: {
+            message: match[4],
+            severity: 'warning',
+            line: match[2] - 1,
+            lineEnd: match[2] - 1,
+            column: match[3] - 1
+        },
+    };
+});
