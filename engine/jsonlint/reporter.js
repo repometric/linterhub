7a641ff9300cc65390f561aab51356358ee29eea
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-05-29 13:28:04 +0300

    Fix errors for linting

--- engine/jsonlint/reporter.js
+++ engine/jsonlint/reporter.js
@@ -1,14 +1,12 @@
-'use strict';
-
 const template = module.exports = {
     run: function(onFinish) {
         process.stdin.resume();
-        process.stdin.setEncoding('utf8');
+        process.stdin.setEncoding("utf8");
 
-        let lingeringLine = '';
+        let lingeringLine = "";
         let lines = [];
         
-        process.stdin.on('data', function(chunk) {
+        process.stdin.on("data", function(chunk) {
             let linesParsed = chunk.split('\n');
             linesParsed[0] = lingeringLine + linesParsed[0];
             lingeringLine = linesParsed.pop();
@@ -19,7 +17,7 @@ const template = module.exports = {
             });
         });
 
-        process.stdin.on('end', function() {
+        process.stdin.on("end", function() {
             onFinish(lines);
         });
       
@@ -30,12 +28,7 @@ const reporter = module.exports = {
     run: function(regex, convertMatch) {
         let results = [];
 
-        template.run(function(lines) {
-            lines.forEach(processLine);
-           console.log(JSON.stringify(results));
-        });
-       
-        /**
+         /**
          * Generates standard model form each line
          * @param {*} line Line to parse
          */
@@ -63,6 +56,11 @@ const reporter = module.exports = {
 
             obj.messages.push(objParsed.message);
         }
+
+        template.run(function(lines) {
+            lines.forEach(processLine);
+            console.log(JSON.stringify(results));
+        });
     },
 };
 let regex = /(.*): line ([0-9]+), col ([0-9]+), (.*)/g;
@@ -72,7 +70,7 @@ reporter.run(regex, function(match) {
         path: match[1].trim(),
         message: {
             message: match[4],
-            severity: 'warning',
+            severity: "warning",
             line: match[2] - 1,
             lineEnd: match[2] - 1,
             column: match[3] - 1
